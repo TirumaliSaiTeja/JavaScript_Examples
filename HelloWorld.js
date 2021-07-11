5f4eeef519f950ac27e78ce2dd3622f566214d5c
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-11 17:57:57 +0530

    Day 1- Hello World

---
+++ HelloWorld.js
@@ -0,0 +1,13 @@
+// there are three ways to print hello world in javascript
+
+// 1st method: Using console.log
+
+console.log("Hello World");
+
+// 2nd method: Using alert()
+
+alert("Hello World");
+
+// 3rd method: Using document.write()
+
+document.write("Hello World");
