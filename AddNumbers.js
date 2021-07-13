3865bb6527c1e63c56fb35fb5569faa335afa823
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-13 23:29:01 +0530

    Day 3- Conversion(km-m)

--- AddNumbers.js
+++ AddNumbers.js
@@ -1,3 +1,5 @@
+// Adding Numbers using JS Arthemetics
+
 const a = 13;
 const b = 14;
 
@@ -10,5 +12,13 @@ console.log(e);
 const f = (a%b);
 console.log(f);
 
+// Numbers entered by the user
+
+const num1 = parseInt(prompt('Please enter a value'));
+const num2 = parseInt(prompt('Please enter a value'));
+
+const sum = num1 + num2;
+
+console.log(`The total value of ${num1} and ${num2} is ${sum}`);
 
 
---
+++ conversion.js
@@ -0,0 +1,7 @@
+let km = parseInt(prompt("Enter a value in kilometers: "));
+
+const factor = 0.621371;
+
+const mile = km * factor;
+
+console.log(`The total ${km} kilometers is equal to ${mile} miles`);
