470ac3d0ee6be517900998f817adccc90dc6677c
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-14 23:39:41 +0530

    Day 4- Checking the number for even or odd

---
+++ comparision.js
@@ -0,0 +1,15 @@
+// Comparision of positive, negative and zero values
+
+let value = prompt("Please enter a number");
+
+if (value>0){
+    console.log("Entered number is a positive number");
+}
+
+else if (value==0){
+    console.log("Entered value is Zero");
+}
+
+else {
+    console.log(" Entered number is a negative number");
+}
---
+++ even_odd.js
@@ -0,0 +1,11 @@
+// Finding the number is even or odd
+
+let number = prompt("Enter a value");
+
+if (number%2==0){
+    console.log("Entered number is a even number");
+}
+
+else {
+    console.log("Entered number is a odd number");
+}
