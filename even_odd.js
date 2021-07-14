8b80a125dabe768be827081bf808a80acd142f25
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-14 23:46:20 +0530

    Day 4 - Finding largest number

--- even_odd.js
+++ even_odd.js
@@ -1,11 +1,33 @@
 // Finding the number is even or odd
 
-let number = prompt("Enter a value");
+// let number = prompt("Enter a value");
 
-if (number%2==0){
-    console.log("Entered number is a even number");
+// if (number%2==0){
+//     console.log("Entered number is a even number");
+// }
+
+// else {
+//     console.log("Entered number is a odd number");
+// }
+
+// Finding the largest number 
+
+let num1 = prompt("Enter the value for num1");
+let num2 = prompt("Enter the value for num2");
+let num3 = prompt("Enter the value for num3");
+
+let largest
+
+if(num1>=num2&&num1>=num3){
+    largest = num1;
+}
+
+else if(num2>=num1&&num2>=num3){
+    largest = num2;
 }
 
 else {
-    console.log("Entered number is a odd number");
+    largest = num3;
 }
+
+console.log(`largest number is ${largest}`);
