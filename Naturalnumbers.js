8608f34177e78d3e1205262734bf743925d57f82
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-16 22:44:13 +0530

    Day-6 natural numbers

---
+++ Naturalnumbers.js
@@ -0,0 +1,12 @@
+// Sum of natural numbers.
+
+let number = prompt("Please enter the value: ");
+
+let sum = 0;
+
+for(let i=1; i<=number; i++){
+    sum = sum + i;
+    
+}
+
+console.log(`Totoal of natural numbers is ${sum}`);
