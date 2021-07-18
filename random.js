22808ee08f7d748f8f0a5c6a5a63160f6370b4ac
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-18 23:01:47 +0530

    Day 7- Sum of natural numbers using recursion

--- random.js
+++ random.js
@@ -3,3 +3,22 @@
 const a = Math.floor(Math.random() *(100-1)+1);
 
 console.log(a);
+
+// Sum of natural numbers using recursion.
+
+function sum(num){
+    if(num>0){
+        return num + sum(num-1);
+    }
+    else{
+        return num;
+    }
+}
+
+// user to pass a number
+
+const number = prompt("Please enter a positive value: ");
+
+const result = sum(number);
+
+console.log(`Sum of natural number is ${result}`);
