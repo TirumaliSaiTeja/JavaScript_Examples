b83510201a422a8a4775fd820fdb2db6c90b204f
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-20 23:16:29 +0530

    Day 9- Finding ascii numbers of characters

---
+++ ascii.js
@@ -0,0 +1,8 @@
+//finding ascii values
+//ascii- american standard code for information interchange
+
+let character = prompt("Enter a character: ");
+
+let result = character.charCodeAt();
+
+console.log(`ascii value of ${character} is ${result}`);
