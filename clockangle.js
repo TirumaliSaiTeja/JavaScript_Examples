3655ea4d1497966deeb5b727040978c976f78a92
TirumaliSaiTeja <tsaiteja7@gmail.com>
2021-07-22 23:41:38 +0530

    Day 10- Simple clock angle

---
+++ clockangle.js
@@ -0,0 +1,12 @@
+function clockangle(num){
+
+
+
+    // We got 6 beacuse 360/60=6
+    // 360 represents the full number of a degree in a circle and 
+    // 60 is the number of minutes on a clock, so dividing these two numbers
+    // gives us the number of the degrees for one minute
+return num*6;
+}
+
+clockangle(6);
