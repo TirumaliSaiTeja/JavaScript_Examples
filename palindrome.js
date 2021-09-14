b9cd8a3eff974e538ccb4005c9d263e9e298e577
Sai Teja <54593666+TirumaliSaiTeja@users.noreply.github.com>
2021-09-14 20:44:54 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,18 +2,18 @@
 
 // Palindrome example is dad, mom, madam
 
-function checkPalin(str){
+// function checkPalin(str){
 
-const len = string.length;
+// const len = string.length;
 
-for(let i=0; i<len/2;i++){
-      // check if first and last string are same
-      if (string[i] !== string[len - 1 - i]) {
-        return 'It is not a palindrome';
-    }
-}
-return 'It is a palindrome';
-}
+// for(let i=0; i<len/2;i++){
+//       // check if first and last string are same
+//       if (string[i] !== string[len - 1 - i]) {
+//         return 'It is not a palindrome';
+//     }
+// }
+// return 'It is a palindrome';
+// }
 
 // take input
 const string = prompt('Enter a string: ');
